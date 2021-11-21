969f01e3cbd0285e35da02d9e41f4246298e0ccf
atherashraf <athear.ashraf@mic3.com>
2021-11-21 19:16:12 +0500

    Project Configuration

--- app/utils/scope_box.py
+++ app/utils/scope_box.py
@@ -10,9 +10,9 @@ class ScopeBox():
 
     def get_global_offeset_of_3D_files(self):
         query = "SELECT ROUND(CAST(tf.globaloffset->>'x' AS NUMERIC), 9)*(-1)," \
-                "ROUND(CAST(tf.globaloffset->>'y' AS NUMERIC), 9)*(-1)," \
-                "ROUND(CAST(tf.globaloffset->>'z' AS NUMERIC), 9)*(-1)" \
-                "FROM tbl_files tf WHERE tf.pk_fileid = p_file_id AND fk_file_type_id = 1;"
+                " ROUND(CAST(tf.globaloffset->>'y' AS NUMERIC), 9)*(-1)," \
+                " ROUND(CAST(tf.globaloffset->>'z' AS NUMERIC), 9)*(-1)" \
+                " FROM tbl_files tf WHERE tf.pk_fileid = p_file_id AND fk_file_type_id = 1;"
         qs = get_raw_query_qs(query)
         for obj in qs:
             print(obj)
